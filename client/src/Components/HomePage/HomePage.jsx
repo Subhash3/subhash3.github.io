f66b68d6d7b4449b72ee7471f4c338403941a069
Subhash Sarangi <subhashsarangi123@gmail.com>
2021-01-28 22:02:29 +0530

    Basic HomePage setup

---
+++ client/src/Components/HomePage/HomePage.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import './HomePage.min.css'
+
+const HomePage = () => {
+    return (
+        <div className="home-page">
+            <div className="personal-info">
+                <div className="name"><span>S</span>ubhash <span>S</span>arangi</div>
+                Full Stack Developer |
+                Deep Learning Enthusiast
+            </div>
+            <div className="logo">S.S</div>
+        </div>
+    );
+}
+
+export default HomePage;

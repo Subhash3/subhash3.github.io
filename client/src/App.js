a9af8a4f0f50d2c3d3be51f1afda786acf80d109
Subhash Sarangi <subhashsarangi123@gmail.com>
2021-01-28 21:59:56 +0530

    Placeholder components

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,11 @@
 import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import NavBar from './Components/NavBar/NavBar'
+import HomePage from './Components/HomePage/HomePage'
+import Experience from './Components/Experience/Experience'
+import GetInTouch from './Components/GetInTouch/GetInTouch'
+import Projects from './Components/Projects/Projects'
+import AboutMe from './Components/AboutMe/AboutMe'
 
 import './App.css';
 
@@ -9,6 +14,13 @@ function App() {
     <div className="App">
       <Router>
         <NavBar />
+        <Switch>
+          <Route exact path="/" component={HomePage} />
+          <Route exact path="/experience" component={Experience} />
+          <Route exact path="/contact" component={GetInTouch} />
+          <Route exact path="/projects" component={Projects} />
+          <Route exact path="/about" component={AboutMe} />
+        </Switch>
       </Router>
     </div>
   );
---
+++ client/src/Components/GetInTouch/GetInTouch.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const GetInTouch = () => {
+    return (
+        <div className="contact">
+            Get in touch
+        </div>
+    );
+}
+
+export default GetInTouch;

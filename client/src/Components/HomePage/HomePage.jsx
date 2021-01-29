39ddf05cbf50af509db06a5928235b30451732f2
Subhash Sarangi <subhashsarangi123@gmail.com>
2021-01-29 10:54:52 +0530

    Changed the colors to fit with the new color scheme

--- client/src/Components/HomePage/HomePage.jsx
+++ client/src/Components/HomePage/HomePage.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+// import FacebookIcon from '@material-ui/icons/Facebook';
 
 import './HomePage.min.css'
 
@@ -9,6 +10,12 @@ const HomePage = () => {
                 <div className="name"><span>S</span>ubhash <span>S</span>arangi</div>
                 Full Stack Developer |
                 Deep Learning Enthusiast
+                <div className="social-links">
+                    <div className="facebook">F</div>
+                    <div className="instagram">I</div>
+                    <div className="github">G</div>
+                    <div className="linkedin">L</div>
+                </div>
             </div>
             <div className="logo">S.S</div>
         </div>

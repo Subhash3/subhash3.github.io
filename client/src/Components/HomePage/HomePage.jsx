5537cdb0661bb9d89f9eaa2b81cb72b6da4eeca1
Subhash Sarangi <subhashsarangi123@gmail.com>
2021-01-30 11:33:31 +0530

    Added stubmle animation to social links

--- client/src/Components/HomePage/HomePage.jsx
+++ client/src/Components/HomePage/HomePage.jsx
@@ -3,27 +3,18 @@ import FacebookIcon from '@material-ui/icons/Facebook';
 import InstagramIcon from '@material-ui/icons/Instagram'
 import GitHubIcon from '@material-ui/icons/GitHub';
 import LinkedInIcon from '@material-ui/icons/LinkedIn';
+import { separateLettersOfName } from '../../Utils/helpers'
 
 import './HomePage.min.css'
 
 const HomePage = () => {
 
-    const separateLettersOfName = (name) => {
-        let len = name.length
-        let lettersArray = []
-        for (let i = 0; i < len; i++) {
-            let className = `letter ${name[i] === name[i].toUpperCase() ? "capital" : ""}`
-            lettersArray.push(<span key={i} className={className}>{name[i]}</span>)
-        }
-
-        return lettersArray
-    }
-
     return (
         <div className="home-page">
             <div className="left-card personal-info">
                 <div className="name">
-                    {separateLettersOfName("Subhash Sarangi").map(letterElement => letterElement)}
+                    {/* {separateLettersOfName("Subhash Sarangi").map(letterElement => letterElement)} */}
+                    {separateLettersOfName("Subhash Sarangi")}
                 </div>
                 <div className="sub-title">Full Stack Developer | Deep Learning Enthusiast</div>
                 <div className="social-links">

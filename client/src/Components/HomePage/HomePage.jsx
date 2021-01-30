72927f09e3dc83f42904fcc2b38f0a6f3c8c92cf
Subhash Sarangi <subhashsarangi123@gmail.com>
2021-01-30 22:26:25 +0530

    Set up basic reveal transitions for home page elemets

--- client/src/Components/HomePage/HomePage.jsx
+++ client/src/Components/HomePage/HomePage.jsx
@@ -4,6 +4,9 @@ import InstagramIcon from '@material-ui/icons/Instagram'
 import GitHubIcon from '@material-ui/icons/GitHub';
 import LinkedInIcon from '@material-ui/icons/LinkedIn';
 import { separateLettersOfName } from '../../Utils/helpers'
+import { Zoom, Fade, Flip, Roll, Rotate, Bounce, Slide } from 'react-reveal'
+import HeadShake from 'react-reveal/HeadShake'
+import Jump from 'react-reveal/Jump'
 
 import './HomePage.min.css'
 
@@ -12,18 +15,24 @@ const HomePage = () => {
     return (
         <div className="home-page">
             <div className="left-card personal-info">
-                <div className={`name`}>
-                    {/* {separateLettersOfName("Subhash Sarangi").map(letterElement => letterElement)} */}
-                    {separateLettersOfName("Subhash Sarangi")}
-                </div>
+                <SlideAndFade>
+                    <div className={`name`}>
+                        {/* {separateLettersOfName("Subhash Sarangi").map(letterElement => letterElement)} */}
+                        {separateLettersOfName("Subhash Sarangi")}
+                    </div>
+                </SlideAndFade>
                 <div className="sub-title">Full Stack Developer | Deep Learning Enthusiast</div>
-                <div className="social-links">
-                    <div className="facebook"><FacebookIcon /></div>
-                    <div className="instagram"><InstagramIcon /></div>
-                    <div className="github"><GitHubIcon /></div>
-                    <div className="linkedin"><LinkedInIcon /></div>
-                </div>
-                <div className="connect-btn">Connect</div>
+                <Jump>
+                    <div className="social-links">
+                        <div className="facebook"><FacebookIcon /></div>
+                        <div className="instagram"><InstagramIcon /></div>
+                        <div className="github"><GitHubIcon /></div>
+                        <div className="linkedin"><LinkedInIcon /></div>
+                    </div>
+                </Jump>
+                <HeadShake delay={500}>
+                    <div className="connect-btn">Connect</div>
+                </HeadShake>
             </div>
             <div className="right-card">
                 <div className="logo">SS</div>
@@ -31,5 +40,7 @@ const HomePage = () => {
         </div>
     );
 }
+
+const SlideAndFade = ({ children }) => <Slide left><Fade>{children}</Fade></Slide>
 
 export default HomePage;

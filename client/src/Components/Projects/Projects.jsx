b207aa90e33698fe9a749755add95fe87ba21f50
Subhash Sarangi <subhashsarangi123@gmail.com>
2021-02-01 18:26:26 +0530

    Projects template

--- client/src/Components/Projects/Projects.jsx
+++ client/src/Components/Projects/Projects.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from '../../Images/logo2.jpeg'
 import { Store } from '../../Redux/Store'
 import { separateLettersOfName } from '../../Utils/helpers'
@@ -52,19 +52,27 @@ const Projects = () => {
 }
 
 const Project = ({ project }) => {
+    const [infoRevealed, setInfoRevealed] = useState(false)
     const { title, info } = project
 
+    const toggleInfo = () => {
+        setInfoRevealed(!infoRevealed)
+    }
+
     return (
-        <div className="project-card">
+        <div className={`project-card  ${infoRevealed ? 'revealed' : ''}`}>
             <div className="thumbnail">
                 <img src={logo} alt={title} />
             </div>
             <div className="title">{title}</div>
-            <div className="info">{info}</div>
+            <div className={`info`}>{info}</div>
             <div className="buttons">
                 {/* <button className="visit-project">Vist Project</button>
                 <button className="visit-repo">Visit Repo</button> */}
-                <button className="read-more">Read More</button>
+                <button
+                    className="read-more"
+                    onClick={toggleInfo}
+                >Read More</button>
             </div>
         </div>
     );

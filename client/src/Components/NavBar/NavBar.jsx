ae4e2fff0a2607ed9dcfa6dc26dc92b289422bf4
Subhash Sarangi <subhashsarangi123@gmail.com>
2021-06-06 17:42:57 +0530

    Finished styling ham burger

--- client/src/Components/NavBar/NavBar.jsx
+++ client/src/Components/NavBar/NavBar.jsx
@@ -1,35 +1,46 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { navItems } from './navItems.js'
-import { Store } from '../../Redux/Store'
+// import { Store } from '../../Redux/Store'
 
 import './NavBar.min.css'
 
 const NavBar = ({ vertical }) => {
-    Store.dispatch({ type: 'hihi', payload: {} })
+    const [hamBurgerActive, setHamBurgerActive] = useState(false)
+
+    console.log(hamBurgerActive)
+
+    const toggleHamBurger = (e) => {
+        setHamBurgerActive(!hamBurgerActive)
+    }
 
     return (
-        <div className={`nav-bar ${vertical ? 'vertical' : ''}`}>
-            <div className="ham-burger">
+        <div className={`nav-bar ${vertical ? 'vertical' : ''} ${hamBurgerActive ? 'active' : ''}`}>
+            <div
+                className="ham-burger"
+                onClick={toggleHamBurger}
+            >
                 <div className="line line1"></div>
                 <div className="line line2"></div>
                 <div className="line line3"></div>
             </div>
-            <div className="nav-link name-logo">
-                <a href="/">
-                    <span className="fname">Subhash</span> Sarangi
+            <div className="nav-items-container">
+                <div className="nav-link name-logo">
+                    <a href="/">
+                        <span className="fname">Subhash</span> Sarangi
                 </a>
-            </div>
-            <div className="nav-items">
-                {navItems.map((item, index) => {
-                    return <NavItem
-                        key={index}
-                        text={item.text}
-                        linkTo={item.linkTo}
-                        className={item.className}
-                        Icon={item.icon}
-                        iconDisplay={vertical}
-                    />
-                })}
+                </div>
+                <div className="nav-items">
+                    {navItems.map((item, index) => {
+                        return <NavItem
+                            key={index}
+                            text={item.text}
+                            linkTo={item.linkTo}
+                            className={item.className}
+                            Icon={item.icon}
+                            iconDisplay={vertical}
+                        />
+                    })}
+                </div>
             </div>
         </div>
     );
@@ -39,7 +50,7 @@ const NavItem = ({ text, linkTo, className, Icon, iconDisplay }) => {
     return (
         <div className={`nav-link ${className}`}>
             {iconDisplay ? <a href={linkTo}><Icon /></a> : null}
-            <a href={linkTo} exact activeClassName='active'>{text}</a>
+            <a href={linkTo}>{text}</a>
         </div>
     )
 }
